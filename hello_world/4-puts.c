2181dc3bbd379b83c070d87507ca9fa03719d51c
atanih <torresalva09@gmail.com>
2026-02-17 11:54:18 -0400

    C program that prints exactly

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+main - prints a string followed by a new line,
+*
+Description: Uses puts to print the exact string,
+""Programming is like building a multilingual puzzle"",
+*
+Return: Always 0,
+*/
+int main(void)
+{
+puts(""Programming is like building a multilingual puzzle");
+return (0);
+}
