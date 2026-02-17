4b4f4f2459b31739147fea25640f24d8c0f5cda5
atanih <torresalva09@gmail.com>
2026-02-17 14:57:59 -0400

    update

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
-main - prints a string followed by a new line,
+*main - prints a string followed by a new line,
 *
 Description: Uses puts to print the exact string,
 ""Programming is like building a multilingual puzzle"",
 *
-Return: Always 0,
+*Return: Always 0,
 */
 int main(void)
 {
-puts(""Programming is like building a multilingual puzzle");
-return (0);
+	puts(""Programming is like building a multilingual puzzle");
+	return (0);
 }
