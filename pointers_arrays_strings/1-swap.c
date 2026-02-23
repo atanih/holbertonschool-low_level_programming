bf8d3801b48fa1d927ea7c72e16f81a255fb3124
atanih <torresalva09@gmail.com>
2026-02-22 21:02:43 -0400

    Update on betty

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/*
+ * swap_int - swapl the value of two integrals
+ * @a: pointers to first integer
+ * @b: pointer to second integer
+ */
 void swap_int(int *a, int *b)
 {
 int temp;
