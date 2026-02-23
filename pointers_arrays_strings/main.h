cbf97b5ef0191a9100ce6f47169771aa3ccb6460
atanih <torresalva09@gmail.com>
2026-02-22 21:24:41 -0400

    Update

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+int _putchar(char c);
 void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 void print_rev(char *s);
