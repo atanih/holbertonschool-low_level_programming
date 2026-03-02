ada73af3e9ff880931178095f6ca53379aba37e7
atanih <torresalva09@gmail.com>
2026-03-01 20:49:30 -0400

    update

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,5 +17,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
