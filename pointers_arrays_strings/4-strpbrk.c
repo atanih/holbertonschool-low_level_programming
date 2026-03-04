5d93effd0a34009cae06d3571be85275c45f5b84
atanih <torresalva09@gmail.com>
2026-03-04 00:36:35 -0400

    Update

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -10,11 +10,12 @@
  */
 char *_strpbrk(char *s,char *accept)
 {
-	int i,j;
-	for(i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; accept[j] != '\0'; j++)
-	}
+int i,j;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+}
 }
 return(0)
 }
