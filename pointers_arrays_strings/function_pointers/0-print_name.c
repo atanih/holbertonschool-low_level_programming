24b0eaf67b0b2e0083f30ecbe1b2762bc832fa99
atanih <torresalva09@gmail.com>
2026-03-12 12:11:09 -0400

    update

--- pointers_arrays_strings/function_pointers/0-print_name.c
+++ pointers_arrays_strings/function_pointers/0-print_name.c
@@ -2,14 +2,16 @@
 #include "function_pointers.h"
 
 /**
- * print_name - print a name
- * @name: name to print 
- * @f: function and pointer
+ * print_name - prints a name using a function pointer
+ * @name: name to print
+ * @f: pointer to function
+ *
+ * Return: nothing
  */
- void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f !- NULL)
-{
-f(name);
-}
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
---
+++ pointers_arrays_strings/function_pointers/README.md
@@ -0,0 +1,8 @@
+# Function Pointers
+
+This project is about learning how to use function pointers in C.
+
+## Concepts
+- What function pointers are
+- How to use them
+- Passing functions as arguments
