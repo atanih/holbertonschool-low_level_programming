2fd2f1d79ef96bc8da9c92d1279e8f52b7b47691
atanih <torresalva09@gmail.com>
2026-04-15 18:27:36 -0400

    Retype code could not find error

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,4 +1,4 @@
-#include "hash_tables.h"
+[6:23 PM, 4/15/2026] AJTorres: #include "hash_tables.h"
 #include <string.h>
 
 /**
@@ -46,6 +46,70 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	node->key = (char *)key;
+	node->value = value_copy;
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
+}
+[6:27 PM, 4/15/2026] AJTorres: #include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: The hash table
+ * @key: The key, string. Key cannot be an empty string
+ * @value: The value associated with the key. value must be duplicated.
+ * value can be an empty string
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	unsigned long int index;
+	char *value_copy;
+	char *key_copy;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = malloc(strlen(value) + 1);
+	if (value_copy == NULL)
+		return (0);
+	strcpy(value_copy, value);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = value_copy;
+			return (1);
+		}
+		node = node->next;
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+
+	key_copy = malloc(strlen(key) + 1);
+	if (key_copy == NULL)
+	{
+		free(value_copy);
+		free(node);
+		return (0);
+	}
+	strcpy(key_copy, key);
+
+	node->key = key_copy;
 	node->value = value_copy;
 	node->next = ht->array[index];
 	ht->array[index] = node;
