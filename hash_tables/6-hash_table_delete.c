7603473bf6808db8c49383eaf46d143fa2768350
atanih <torresalva09@gmail.com>
2026-04-15 18:53:13 -0400

    Update on code lines and add include <stdlib.h>

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,33 +1,31 @@
 #include <stdlib.h>
-#include “hash_tables.h”
+#include "hash_tables.h"
 
 /**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *tmp;
 
-- hash_table_delete - Deletes a hash table
-- @ht: The hash table
-  */
-  void hash_table_delete(hash_table_t *ht)
-  {
-  hash_node_t *node;
-  hash_node_t *temp;
-  unsigned long int i;
-  
-  if (ht == NULL)
-  return;
-  
-  for (i = 0; i < ht->size; i++)
-  {
-  node = ht->array[i];
-  while (node != NULL)
-  {
-  temp = node->next;
-  free(node->key);
-  free(node->value);
-  free(node);
-  node = temp;
-  }
-  }
-  
-  free(ht->array);
-  free(ht);
-  }
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
