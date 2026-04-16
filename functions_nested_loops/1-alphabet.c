4ca2b4d36140ef0f8830bfebe9d155d3605bc753
atanih <torresalva09@gmail.com>
2026-04-15 21:59:22 -0400

    Update

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,22 +7,11 @@
  */
 void print_alphabet(void)
 {
-	char letra;
+	char letter;
 
-	for (letra = 'a'; letra <= 'z'; letra++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		_putchar(letra);
+		_putchar(letter);
 	}
 	_putchar('\n');
-}
-
-/**
- * main - entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
