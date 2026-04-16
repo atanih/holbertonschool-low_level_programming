2a98de0642beccddeb60c43999e4193fd4d5fb97
atanih <torresalva09@gmail.com>
2026-04-16 02:07:28 -0400

    Fix main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,7 +6,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-void print_sign(int n);
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void print_times_table(int n);
