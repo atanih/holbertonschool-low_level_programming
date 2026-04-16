d00e00e02f17b650e489bc890b7c0a9a93623b70
atanih <torresalva09@gmail.com>
2026-04-15 21:40:56 -0400

    Update

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -20,7 +20,7 @@ int _putchar(char c)
  */
 int main(void)
 {
-	putchar('');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -29,5 +29,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+
+return (0);
 }
