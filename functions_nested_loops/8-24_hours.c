d3818830b35a495831d29b49ef35cfff39fb986e
atanih <torresalva09@gmail.com>
2026-04-20 22:17:18 -0400

    Fix 8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include “main.h”
+#include "main.h"
 
 /**
 *jack_bauer - prints every minute of the day of Jack Bauer 
@@ -17,13 +17,13 @@
   minute = 0;
   while (minute < 60)
   {
-  time_str[0] = ‘0’ + (hour / 10);
-  time_str[1] = ‘0’ + (hour % 10);
-  time_str[2] = ‘:’;
-  time_str[3] = ‘0’ + (minute / 10);
-  time_str[4] = ‘0’ + (minute % 10);
+  time_str[0] = '0' + (hour / 10);
+  time_str[1] = '0' + (hour % 10);
+  time_str[2] = ':';
+  time_str[3] = '0' + (minute / 10);
+  time_str[4] = '0' + (minute % 10);
   write(1, time_str, 5);
-  write(1, “\n”, 1);
+  write(1, '\n', 1);
   minute++;
   }
   hour++;
