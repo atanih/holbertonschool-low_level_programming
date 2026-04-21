a34a551ce66df73bc4e51ffbcbd26d42d5bc22a0
atanih <torresalva09@gmail.com>
2026-04-20 22:21:27 -0400

    Fix betty

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-*jack_bauer - prints every minute of the day of Jack Bauer 
+*jack_bauer - prints every minute of the day of Jack Bauer
 *Return: void
 */
 void jack_bauer(void)
