5971705f4e3dfd6c201b4ba128b73afbc4e5b3df
atanih <torresalva09@gmail.com>
2026-04-22 20:07:15 -0400

    Write a function that prints a string, followed by a new line.

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,5 +24,12 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
 
 #endif
