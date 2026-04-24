2ac68bce19faea1a5631dcc08b045fff0e562982
atanih <torresalva09@gmail.com>
2026-04-24 02:20:10 -0400

    Creat dog.h and add README.md

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - A dog's essential information
+*@name: The dog's name
+*@age: The dog's age
+*@owner: The owner's name
+*Description: Stores basic information about a dog
+*/
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+#endif
