93b88f7dae746676e2afd9adff2516691b76bab6
atanih <torresalva09@gmail.com>
2026-04-24 01:57:41 -0400

    No more line

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,25 @@
 #include <string.h>
 
 /**
+* _strlen - Returns the length of a string
+* @s: String to measure
+*
+* Return: Length of string
+*/
+unsigned int _strlen(char *s)
+{
+unsigned int len = 0;
+
+if (s == NULL)
+return (0);
+
+while (s[len])
+len++;
+
+return (len);
+}
+
+/**
 * string_nconcat - Concatenates two strings with n bytes limit from s2
 * @s1: First string (or NULL)
 * @s2: Second string (or NULL)
@@ -15,20 +34,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int len_s1, len_s2, i, j;
 char *result;
 
-len_s1 = 0;
-len_s2 = 0;
-
-if (s1 != NULL)
-{
-while (s1[len_s1])
-len_s1++;
-}
-
-if (s2 != NULL)
-{
-while (s2[len_s2])
-len_s2++;
-}
+len_s1 = _strlen(s1);
+len_s2 = _strlen(s2);
 
 if (n >= len_s2)
 n = len_s2;
