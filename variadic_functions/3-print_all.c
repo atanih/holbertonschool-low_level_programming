96e3b236b8dead55a131783a76ca549376b14b81
atanih <torresalva09@gmail.com>
2026-04-24 06:05:47 -0400

    Update on code

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,6 +3,28 @@
 #include <stdio.h>
 
 /**
+* has_next - Checks if there's a valid format character after position i
+* @format: The format string
+* @i: Current position
+*
+* Return: 1 if there's another valid char, 0 otherwise
+*/
+int has_next(const char *format, int i)
+{
+int j;
+
+j = i + 1;
+while (format[j])
+{
+if (format[j] == 'c' || format[j] == 'i'
+|| format[j] == 'f' || format[j] == 's')
+return (1);
+j++;
+}
+return (0);
+}
+
+/**
 * print_all - Prints anything
 * @format: A list of types of arguments passed to the function
 *
@@ -12,7 +34,6 @@ void print_all(const char *format, ...)
 {
 va_list args;
 int i;
-int j;
 char *str;
 
 va_start(args, format);
@@ -23,29 +44,17 @@ switch (format[i])
 {
 case 'c':
 printf("%c", va_arg(args, int));
-j = i + 1;
-while (format[j] && format[j] != 'c' && format[j] != 'i'
-&& format[j] != 'f' && format[j] != 's')
-j++;
-if (format[j] != '\0')
+if (has_next(format, i))
 printf(", ");
 break;
 case 'i':
 printf("%d", va_arg(args, int));
-j = i + 1;
-while (format[j] && format[j] != 'c' && format[j] != 'i'
-&& format[j] != 'f' && format[j] != 's')
-j++;
-if (format[j] != '\0')
+if (has_next(format, i))
 printf(", ");
 break;
 case 'f':
 printf("%f", va_arg(args, double));
-j = i + 1;
-while (format[j] && format[j] != 'c' && format[j] != 'i'
-&& format[j] != 'f' && format[j] != 's')
-j++;
-if (format[j] != '\0')
+if (has_next(format, i))
 printf(", ");
 break;
 case 's':
@@ -53,11 +62,7 @@ str = va_arg(args, char *);
 if (str == NULL)
 str = "(nil)";
 printf("%s", str);
-j = i + 1;
-while (format[j] && format[j] != 'c' && format[j] != 'i'
-&& format[j] != 'f' && format[j] != 's')
-j++;
-if (format[j] != '\0')
+if (has_next(format, i))
 printf(", ");
 break;
 }
